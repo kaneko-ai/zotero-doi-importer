c432853a4596dc26b84879c1c4d809b5dc1ef40d
kaneko-ai <kanekiti1125@gmail.com>
2025-04-22 03:25:33 +0900

    Update zotero.py

--- services/zotero.py
+++ services/zotero.py
@@ -1,10 +1,16 @@
 from pyzotero import zotero
 import os
+from dotenv import load_dotenv
 
+# .env を読み込み
+load_dotenv()
+
+# 環境変数から API キーなどを取得
 ZOTERO_API_KEY = os.getenv("ZOTERO_API_KEY")
 ZOTERO_USER_ID = os.getenv("ZOTERO_USER_ID")
-ZOTERO_LIBRARY_TYPE = "user"
+ZOTERO_LIBRARY_TYPE = "user"  # 個人アカウントの場合は "user"、グループの場合は "group"
 
+# PyZotero オブジェクト生成
 zot = zotero.Zotero(ZOTERO_USER_ID, ZOTERO_LIBRARY_TYPE, ZOTERO_API_KEY)
 
 def register_doi_in_zotero(doi: str) -> bool:
