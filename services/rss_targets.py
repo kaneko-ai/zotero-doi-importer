b5d676e43f66edc1ebd498e46c3b3d0c715c07f2
kaneko-ai <kanekiti1125@gmail.com>
2025-04-22 03:53:00 +0900

    Create rss_targets.py

---
+++ services/rss_targets.py
@@ -0,0 +1,5 @@
+RSS_TARGETS = {
+    "Nature": "https://www.nature.com/nature.rss",
+    "Science": "https://www.science.org/action/showFeed?type=etoc&feed=rss&jc=science",
+    "Cell": "https://www.cell.com/cell/current.rss"
+}
